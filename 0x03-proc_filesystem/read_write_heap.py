1a51692a2e278bbc9159088c75ecdf9a21685ebb
lh1008 <paintluis@protonmail.com>
2021-03-01 11:37:09 -0500

    Modified to main

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,121 +1,98 @@
 #!/usr/bin/env python3
 '''
-Locates and replaces the first occurrence of a string in the heap
-of a process
-
+Module that locates and replaces the first ocurrence of a string
+in the heap of a process.
 Usage: ./read_write_heap.py PID search_string replace_by_string
-Where:
-- PID is the pid of the target process
-- search_string is the ASCII string you are looking to overwrite
-- replace_by_string is the ASCII string you want to replace
-  search_string with
+
+PID is the pid of the target process
+search_string with
 '''
 
 import sys
 
 
-def print_usage_and_exit():
+def print_usage_exit():
     """ Method to print and exit """
     print('Usage: {} pid search write'.format(sys.argv[0]))
     sys.exit(1)
 
 
-# check usage
-if len(sys.argv) != 4:
-    print_usage_and_exit()
+def main():
+    if len(sys.argv) != 4:
+        print_usage_exit()
 
-# get the pid from args
-pid = int(sys.argv[1])
-if pid <= 0:
-    print_usage_and_exit()
-search_string = str(sys.argv[2])
-if search_string == "":
-    print_usage_and_exit()
-write_string = str(sys.argv[3])
-if search_string == "":
-    print_usage_and_exit()
+    pid = int(sys.argv[1])
+    if pid <= 0:
+        print_usage_exit()
+    search_string = str(sys.argv[2])
+    write_string = str(sys.argv[3])
+    if search_string  == "":
+        print_usage_and_exit()
 
-# open the maps and mem files of the process
-maps_filename = "/proc/{}/maps".format(pid)
-print("[*] maps: {}".format(maps_filename))
-mem_filename = "/proc/{}/mem".format(pid)
-print("[*] mem: {}".format(mem_filename))
+    map_file_name = '/proc/{}/maps'.format(pid)
+    mem_file_name = '/proc/{}/mem'.format(pid)
 
-# try opening the maps file
-try:
-    maps_file = open('/proc/{}/maps'.format(pid), 'r')
-except IOError as e:
-    print("[ERROR] Can not open file {}:".format(maps_filename))
-    print("        I/O error({}): {}".format(e.errno, e.strerror))
-    sys.exit(1)
-
-for line in maps_file:
-    sline = line.split(' ')
-    # check if we found the heap
-    if sline[-1][:-1] != "[heap]":
-        continue
-    print("[*] Found [heap]:")
-
-    # parse line
-    addr = sline[0]
-    perm = sline[1]
-    offset = sline[2]
-    device = sline[3]
-    inode = sline[4]
-    pathname = sline[-1][:-1]
-    print("\tpathname = {}".format(pathname))
-    print("\taddresses = {}".format(addr))
-    print("\tpermisions = {}".format(perm))
-    print("\toffset = {}".format(offset))
-    print("\tinode = {}".format(inode))
-
-    # check if there is read and write permission
-    if perm[0] != 'r' or perm[1] != 'w':
-        print("[*] {} does not have read/write permission".format(pathname))
-        maps_file.close()
-        exit(0)
-
-    # get start and end of the heap in the virtual memory
-    addr = addr.split("-")
-    if len(addr) != 2:  # never trust anyone, not even your OS :)
-        print("[*] Wrong addr format")
-        maps_file.close()
-        exit(1)
-    addr_start = int(addr[0], 16)
-    addr_end = int(addr[1], 16)
-    print("\tAddr start [{:x}] | end [{:x}]".format(addr_start, addr_end))
-
-    # open and read mem
     try:
-        mem_file = open(mem_filename, 'rb+')
+        map_file = open('/proc/{}/maps'.format(pid), 'r')
     except IOError as e:
-        print("[ERROR] Can not open file {}:".format(mem_filename))
-        print("        I/O error({}): {}".format(e.errno, e.strerror))
-        maps_file.close()
-        exit(1)
+        print('[ERROR] Cannot open file {}'.format(map_file_name))
+        sys.exit(1)
 
-    # read heap
-    mem_file.seek(addr_start)
-    heap = mem_file.read(addr_end - addr_start)
+    for line in map_file:
+        split_line = line.split(' ')
+        if split_line[-1][:-1] != "[heap]":
+            continue
+        mem_addr = split_line[0]
+        perms = split_line[1]
+        offset = split_line[2]
+        device = split_line[3]
+        inode = split_line[4]
+        pathname = split_line[-1][:-1]
 
-    # find string
-    try:
-        i = heap.index(bytes(search_string, "ASCII"))
-    except Exception:
-        print("Can't find '{}'".format(search_string))
-        maps_file.close()
+        #   CHECK FOR READ AND WRITE PERMISSION
+        if perms[0] != 'r' or perms[1] != 'w':
+            print('{} does not have read/write permission'.format(pathname))
+            raise PermissionError
+
+        # Get Start and end of the heap in virtual memory
+        address = mem_addr.split('-')
+        if len(address) != 2:
+            print('Wrong address format')
+            map_file.close()
+            exit(1)
+
+        addr_start = int(address[0], 16)
+        addr_end = int(address[1], 16)
+
+        #   OPEN AND READ MEMORY
+        try:
+            mem_file = open(mem_file_name, 'rb+')
+        except IOError as e:
+            print('[ERROR] Cannot open file {}'.format(mem_file_name))
+            map_file.close()
+            exit(1)
+
+        #   READ HEAP
+        mem_file.seek(addr_start)
+        heap = mem_file.read(addr_end - addr_start)
+
+        #   FIND STRING
+        try:
+            i = heap.index(bytes(search_string, 'ASCII'))
+        except Exception:
+            print("Can't find '{}'".format(search_string))
+            map_file.close()
+            mem_file.close()
+            exit(1)
+
+        # write the new string
+        print("Writing '{}' at {:x}".format(write_string, addr_start + i))
+        mem_file.seek(addr_start + i)
+        mem_file.write(bytes(write_string, "ASCII"))
+
+        map_file.close()
         mem_file.close()
-        exit(0)
-    print("[*] Found '{}' at {:x}".format(search_string, i))
+        break
 
-    # write the new string
-    print("[*] Writing '{}' at {:x}".format(write_string, addr_start + i))
-    mem_file.seek(addr_start + i)
-    mem_file.write(bytes(write_string, "ASCII"))
-
-    # close files
-    maps_file.close()
-    mem_file.close()
-
-    # there is only one heap in our example
-    break
+if __name__ == '__main__':
+    main()
