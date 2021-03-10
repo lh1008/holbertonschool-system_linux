6b26e56f454ed11dca9d1d036c38e31ad177a212
lh1008 <paintluis@protonmail.com>
2021-03-10 15:46:43 -0500

    Added Task 2, modified Makefile

---
+++ 0x04-readelf/2-hreadelf.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry to main
+ * Desc: main function to print ELF sections' header
+ * @argc: argument count
+ * @argv: arguments
+ * @env:  environment
+ * Return: -1 in fail and 0 in success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	if (argc != 2)
+		exit(0);
+
+	args[3] = argv[1];
+	if (execve(args[0], args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
