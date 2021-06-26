56d48380200e7e36bc48362553274587abb3d0a0
lh1008 <paintluis@protonmail.com>
2021-06-26 15:17:57 -0500

    Task 0 function thread_entry added

---
+++ 0x0A-multithreading/0-thread_entry.c
@@ -0,0 +1,12 @@
+#include "multithreading.h"
+
+/**
+ * thread_entry - entry to thread entry
+ * Desc: thread_entry function that will serve as
+ * the entry point to a new thread
+ * @arg: void pointer that holds the address of a string
+ * Return: nothing, void function
+ */
+void *thread_entry(void *arg)
+{
+}
