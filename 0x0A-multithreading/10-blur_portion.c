75b97825bca97fdce8c67e755d1ce575c5ef2093
lh1008 <paintluis@protonmail.com>
2021-06-27 10:53:09 -0500

    Added task 2 function file

---
+++ 0x0A-multithreading/10-blur_portion.c
@@ -0,0 +1,12 @@
+#include "multithreading.h"
+
+/**
+ * blur_portion - entry to blu portion
+ * Desc: blu_portion function that blurs
+ * a portion of an image using a Guassian Blur
+ * @portion: pointer to a data structure
+ * Return: new blur file created
+ */
+void blur_portion(blur_portion_t const *portion)
+{
+}
