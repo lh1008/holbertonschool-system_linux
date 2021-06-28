69276014332e33ac2c6420b3906a4be86fa8c3a6
lh1008 <paintluis@protonmail.com>
2021-06-28 18:42:50 -0500

    Fixed print

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -33,12 +33,14 @@ void end(void)
 int tprintf(char const *format, ...)
 {
 	va_list args;
+	int shared;
 
 	va_start(args, format);
 
-	printf("[%ld] %s", pthread_self(), format);
-
 	pthread_mutex_lock(&lock_x);
+
+	shared = printf("[%lu] ", pthread_self());
+	shared += vprintf(format, args);
 
 	pthread_mutex_unlock(&lock_x);
 
