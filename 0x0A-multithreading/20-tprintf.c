f725863b8d4688ede6577e34a9d6b6413575d05f
lh1008 <paintluis@protonmail.com>
2021-06-28 18:18:57 -0500

    Added constructor and destructor

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -1,5 +1,9 @@
 #include "multithreading.h"
 
+pthread_mutex_t lock_x;
+void begin(void) __attribute__((constructor));
+void end(void) __attribute__((destructor));
+
 /**
  * tprintf - entry to printf
  * Desc: tprintf function that uses the printf
@@ -15,7 +19,32 @@ int tprintf(char const *format, ...)
 
 	printf("[%ld] %s", pthread_self(), format);
 
+	pthread_mutex_lock(&lock_x);
+
 	va_end(args);
 
+	pthread_mutex_unlock(&lock_x);
+
 	return (0);
+}
+
+/**
+ * begin - entry to begin constructor
+ * Desc: begin function that starts a mutex init before
+ * executing main
+ * Return: nothing, void function
+ */
+void begin(void)
+{
+	pthread_mutex_init(&lock_x, NULL);
+}
+
+/**
+ * end - entry to end destructor
+ * Desc: end function that exits a mutex init
+ * Return: nothing, void function
+ */
+void end(void)
+{
+	pthread_exit(NULL);
 }
