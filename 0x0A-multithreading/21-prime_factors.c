77aa934209e83eecf9982fe3d4c0b76bd6c8e38a
lh1008 <paintluis@protonmail.com>
2021-07-02 12:13:31 -0500

    Adding task 5 file function, number to primer factors

---
+++ 0x0A-multithreading/21-prime_factors.c
@@ -0,0 +1,13 @@
+#include "multithreading.h"
+
+/**
+ * primer_factors - entry to prime factors
+ * Desc: primer_factors function that factorizes a
+ * number into a list of prime factors
+ * @s: pointer to the string representation of the number to factorize
+ * Return: pointer to factorized string
+ */
+list_t *prime_factors(char const *s)
+{
+	
+}
