0a5516f20189edb24993e4bc1b595698d4ebd85a
lh1008 <paintluis@protonmail.com>
2021-07-02 12:04:20 -0500

    Added task 5 function prototype to header

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -79,5 +79,6 @@ void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 void begin(void) __attribute__((constructor));
 void end(void) __attribute__((destructor));
+list_t *prime_factors(char const *s);
 
 #endif /* MULTITHREADING_H */
