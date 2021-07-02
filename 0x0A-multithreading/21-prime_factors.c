84ae1747688cd50758953d53894251cf6acc27cf
lh1008 <paintluis@protonmail.com>
2021-07-02 15:08:06 -0500

    Finished prime factor function

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -1,4 +1,41 @@
-#include "multithreading.h"
+#include "list.h"
+
+/**
+* primeFactors - built the list of prime factors
+* @n: number to get the prime factors
+* @l: the list of prime factors
+* Return: Nothing
+*/
+void prime_factor(unsigned long n, list_t *list)
+{
+	unsigned long i = 0, *number = NULL;
+
+	while (n % 2 == 0)
+	{
+		number = calloc(1, sizeof(unsigned long));
+		*number = 2;
+		list_add(list, number);
+		n /= 2;
+	}
+	i = 3;
+	while (i * i <= n)
+	{
+		while (n % i == 0)
+		{
+			number = calloc(1, sizeof(unsigned long));
+			*number = i;
+			list_add(list, number);
+			n /= i;
+		}
+		i += 2;
+	}
+	if (n > 2)
+	{
+		number = calloc(1, sizeof(unsigned long));
+		*number = n;
+		list_add(list, number);
+	}
+}
 
 /**
  * primer_factors - entry to prime factors
@@ -9,5 +46,21 @@
  */
 list_t *prime_factors(char const *s)
 {
-	
+	list_t *list;
+	int i = 0;
+	unsigned long n = 0;
+
+	if (s == NULL)
+		return (NULL);
+	list = malloc(sizeof(*list));
+	if (list == NULL)
+		return (NULL);
+	list = list_init(list);
+
+	while (s[i])
+	{
+		n = (n * 10) + (s[i++] - '0');
+	}
+	prime_factor(n, list);
+	return (list);
 }
