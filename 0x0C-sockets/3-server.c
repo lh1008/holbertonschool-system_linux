4f2375ac2d6c2b414e625e643ce664042051940e
lh1008 <paintluis@protonmail.com>
2021-08-07 18:54:41 -0500

    Missing client struct client_addr

--- 0x0C-sockets/3-server.c
+++ 0x0C-sockets/3-server.c
@@ -22,8 +22,8 @@ void socket_error(char *err)
 int main(void)
 {
 	int servSock, status, ac_cept, client_msg;
-	struct sockaddr_in echoServPort;
-	int addrlen = sizeof(echoServPort);
+	struct sockaddr_in echoServPort, client_addr;
+	socklen_t client_addrlen;
 	char buffer[1024] = {0};
 
 	servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -40,22 +40,15 @@ int main(void)
 	if (status < 0)
 		socket_error("listen failed");
 	printf("Server listening to port %d\n", ntohs(echoServPort.sin_port));
-	ac_cept = accept(servSock, (struct sockaddr *) &echoServPort,
-			 (socklen_t *) &addrlen);
+	ac_cept = accept(servSock, (struct sockaddr *) &client_addr,
+			 (socklen_t *) &client_addrlen);
 	if (ac_cept < 0)
 		socket_error("accept failed");
-	printf("Client connected: %s\n", inet_ntoa(echoServPort.sin_addr));
-	printf("Message received: \"");
-	for (;; memset(buffer, '\0', 1024))
-	{
-		client_msg = recv(ac_cept, buffer, 1024 - 1, 0);
-		if (client_msg < 0)
-			socket_error("received failed");
-		else if (client_msg == 0)
-			break;
-		printf("%s", buffer);
-	}
-	printf("\"\n");
+	printf("Client connected: %s\n", inet_ntoa(client_addr.sin_addr));
+	client_msg = recv(ac_cept, buffer, 1024, 0);
+	if (client_msg < 0)
+		socket_error("received failed");
+	printf("Message received: \"%s\n", buffer);
 	close(ac_cept), close(servSock);
 	return (0);
 }
