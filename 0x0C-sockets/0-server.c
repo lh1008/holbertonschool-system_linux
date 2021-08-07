eebc43d92d0d4745009b90c794029021a48e4369
lh1008 <paintluis@protonmail.com>
2021-08-07 11:35:13 -0500

    Trying again

--- 0x0C-sockets/0-server.c
+++ 0x0C-sockets/0-server.c
@@ -7,11 +7,16 @@
  */
 int main(void)
 {
-	int sockid, status, ac_cept;
+	int sockid, status;
 	struct sockaddr_in addrport;
-	int addrlen = sizeof(addrport);
 
-	sockid = socket(PF_INET, SOCK_STREAM, 0);
+	sockid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (sockid < 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
 	addrport.sin_family = AF_INET;
 	addrport.sin_port = htons(PORT);
@@ -23,21 +28,18 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	status = listen(sockid, 1);
+
+	status = listen(sockid, 3);
 
 	if (status < 0)
 	{
-		perror("listen");
+		perror("listen failed");
 		exit(EXIT_FAILURE);
 	}
 
 	printf("Server listening to port %d\n", ntohs(addrport.sin_port));
+	while (1)
+		;
 
-	ac_cept = accept(sockid, (struct sockaddr *) &addrport,
-			 (socklen_t *) &addrlen);
-	if (ac_cept < 0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
+	return (0);
 }
